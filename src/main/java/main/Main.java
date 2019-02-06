4c63e30a3fea8fba91d3a2f5ba75a28d19664709
kazakov-an <kazakov.a.niko@sberbank.ru>
2019-02-06 17:56:19 +0300

    add use jdbc connection to oracleDB

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,8 +1,47 @@
 package main;
 
-public class Main{
-    public static void main(String[] args) {
+import java.sql.*;
+//import java.util.HashMap;
+//import java.util.Map;
+//import java.sql.*;
+//import oracle.jdbc.OracleDriver;
 
+public class Main{
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        String url="jdbc:oracle:thin:@//localhost:1523/orcl.sigma.sbrf.ru";
+        String username="SRVPRJCTCNNCTN";
+        String password="SRVPRJCTCNNCTN";
+        Class.forName("oracle.jdbc.OracleDriver");
+        Connection conn = DriverManager.getConnection(url,username,password);
+        try {
+            Statement stmt = conn.createStatement();
+            try {
+                ResultSet rset = stmt.executeQuery("select * from SRVPRJCT.TBL");
+                try {
+                    String[] collumnNames = {"id", "name"};
+                    while (rset.next()) {
+                        StringBuilder res = new StringBuilder("[" + rset.getRow() + "]");
+                        for (String colName : collumnNames) {
+                            res.append(" ");
+                            res.append(colName);
+                            res.append(":");
+                            res.append(rset.getString(colName));
+                        }
+                        System.out.println(res.toString());
+                    }
+                }
+                finally {
+                    try { rset.close(); } catch (Exception ignore) {}
+                }
+            }
+            finally {
+                try { stmt.close(); } catch (Exception ignore) {}
+            }
+        }
+        finally {
+            try { conn.close(); } catch (Exception ignore) {}
+        }
     }
+
 }
 
---
+++ srvprjct.bat
@@ -0,0 +1,2 @@
+java -jar srvprjct.jar
+pause
